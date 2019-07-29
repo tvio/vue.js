93414deb662a7571e61800a921cbffaeed4cdb4d
tvio <tvio@centrum.cz>
2019-07-29 15:57:22 +0200

    monster parba dodelane funkce

--- monster/mapp.js
+++ monster/mapp.js
@@ -11,10 +11,40 @@ new Vue({
         (this.monsterHealth = 100),
         (this.gameIsRunning = false);
     },
-    attack: function() {
-      this.playerHealth = this.playerHealth - Math.floor(Math.random() * 10);
-      this.monsterHealth = this.monsterHealth - Math.floor(Math.random() * 10);
+
+    dmg: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
-    specialAttack: {}
+    checkDeath: function() {
+      if (this.monsterHealth <= 0) {
+        //confirm kontroluje potvrzeni + prida alert
+        if (confirm("Vyhrál jsi")) {
+          this.reset();
+        } else this.gameIsRunning = false;
+      } else if (this.playerHealth <= 0) {
+        alert("Prohrál jsi");
+        this.reset();
+      }
+    },
+
+    attack: function() {
+      var damage = this.dmg();
+      this.playerHealth -= this.dmg(4, 10);
+      this.monsterHealth -= this.dmg(2, 10);
+      this.checkDeath();
+    },
+    specialAttack: function() {
+      this.playerHealth -= this.dmg(4, 10);
+      this.monsterHealth -= this.dmg(2, 10) * 2;
+      this.checkDeath();
+    },
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth -= this.dmg(5, 10);
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth -= this.dmg(5, 10);
+      }
+    }
   }
 });
